369cdbf61b45b354b90cd36db48efc90c276a247
Alexander Hendrix <a02365406@usu.edu>
2022-03-19 12:53:28 -0600

    working on inventory

--- src/Character.py
+++ src/Character.py
@@ -1,12 +1,17 @@
 
 from src.Health import Health
+from src.Items.Weapons import Weapons
+from src.UserInput import UserInput
+
 
 class character:
     def __init__(self,name,description,health):
+        self.weapons = Weapons()
         self.name = name
         self.description = description
         self.health = Health(health)
-        self.equippedItem = ""
+        self.equippedItem = self.weapons.getWeaponByName("Flimsy Sword")
+        self.inventory = []
 
     def fullDesc(self):
         return f"{self.name}: {self.description}\n\t\tTheir health is: {self.health.health}"
@@ -14,6 +19,27 @@ class character:
     def __str__(self):
         return self.name
 
+    def getCharacterDamage(self):
+        return self.equippedItem.determineDamage()
 
+    def setWeapon(self, weapon):
+        self.equippedItem = weapon
+
+    def addItemToInventory(self, item):
+        self.inventory += item
+
+    def printInventory(self):
+        print(f"\n{self.name}'s Inventory")
+        i = 0
+        for item in self.inventory:
+            i = i + 1
+            print(f"\t[{i}] {item.name} | min dmg: {item.minDamage} | max dmg: {item.maxDamage}")
+
+    def inventoryTUI(self):
+        self.printInventory()
+        emptyStrings = []
+        for i in range():
+            emptyStrings.append("")
+        inputObj = UserInput("",len(self.inventory),emptyStrings)
 
 
