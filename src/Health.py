cda7486ba3e21fe6739b32e68bd805e811b5858c
Alexander Hendrix <a02365406@usu.edu>
2022-03-18 21:42:29 -0600

    constructor methods for Health Class

--- src/Health.py
+++ src/Health.py
@@ -1,3 +1,14 @@
 
-class health:
+class Health:
+    health = 100
+    armor = 0
+
+    def __init__(self, health, armor):
+        self.health = health
+        self.armor = armor
+
+    def __int__(self, health):
+        self.__init__(health, 0)
+
     def __init__(self):
+        self.__int__(100)
