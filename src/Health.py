dfc2319ad016840cf0290ce1439aa6b2003649f7
Alexander Hendrix <a02365406@usu.edu>
2022-03-18 21:56:58 -0600

    Adding Helper Functions to Health class

--- src/Health.py
+++ src/Health.py
@@ -1,14 +1,36 @@
 
 class Health:
     health = 100
+    maxHealth = 100
     armor = 0
+    maxArmor = 0
 
-    def __init__(self, health, armor):
-        self.health = health
+    def __init__(self, maxHealth, maxArmor, health, armor):
+        self.maxHealth = maxHealth
+        self.maxArmor = maxArmor
         self.armor = armor
+        self.health = health
 
-    def __int__(self, health):
-        self.__init__(health, 0)
+    def __init__(self, maxHealth, maxArmor, health):
+        self.__init__(maxHealth, maxArmor, health, maxArmor)
+
+    def __init__(self, maxHealth, maxArmor):
+        self.__int__(maxHealth, maxArmor, maxHealth)
+
+    def __int__(self, maxHealth):
+        self.__init__(maxHealth, 0)
 
     def __init__(self):
         self.__int__(100)
+
+    def addHealth(self, addHealth):
+        proposedNewHealth = self.health + addHealth
+        if proposedNewHealth > self.maxHealth:
+            self.health = self.maxHealth
+        else:
+            self.health = proposedNewHealth
+
+    def subHealth(self, subHealth):
+        proposedNewHealth = self.health - subHealth
+        self.health = proposedNewHealth
+
